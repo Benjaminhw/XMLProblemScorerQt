037dde56089c84502ac81bfdc85acff955384297
MZ_Benjamin <ben199709@foxmail.com>
2023-04-04 18:06:34 +0800

    xml

---
+++ problemxmlparser.cpp
@@ -0,0 +1,5 @@
+﻿#include "problemxmlparser.h"
+
+ProblemXMLParser::ProblemXMLParser()
+{
+}
---
+++ problemxmlparser.h
@@ -0,0 +1,11 @@
+#ifndef PROBLEMXMLPARSER_H
+#define PROBLEMXMLPARSER_H
+
+
+class ProblemXMLParser
+{
+public:
+    ProblemXMLParser();
+};
+
+#endif // PROBLEMXMLPARSER_H
